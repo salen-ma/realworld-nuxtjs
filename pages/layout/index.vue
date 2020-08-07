cb163062b69244d9d57f86e765cd88713e7a1359
mashaoliang <salen.ma@qq.com>
2020-08-07 23:47:29 +0800

    feat: init realworld-nuxtjs

---
+++ pages/layout/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <!-- 顶部导航 -->
+    <nav class="navbar navbar-light">
+      <div class="container">
+        <nuxt-link to="/"
+          class="navbar-brand">conduit</nuxt-link>
+        <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item">
+            <!-- Add "active" class when you're on that page" -->
+            <nuxt-link to="/"
+              exact
+              class="nav-link">Home</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/editor" class="nav-link">
+              <i class="ion-compose"></i>&nbsp;New Post
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/settings" class="nav-link">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/login" class="nav-link">Sign in</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/register" class="nav-link">Sign up</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/profile/liang" class="nav-link">
+              <img class="user-pic" src="https://avatars1.githubusercontent.com/u/23401794?s=60&v=4">
+              liang_
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- 顶部导航 -->
+
+    <!-- 子路由 -->
+    <nuxt-child />
+    <!-- 子路由 -->
+
+    <!-- 底部 -->
+    <footer>
+      <div class="container">
+        <a href="/" class="logo-font">conduit</a>
+        <span class="attribution">
+          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+        </span>
+      </div>
+    </footer>
+    <!-- 底部 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex'
+}
+</script>
